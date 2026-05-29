65255587466fc2ca9d08c471b6ce43ab49c7b375
ParthKhare21 <khare.parth2112@gmail.com>
2026-05-29 22:15:32 +0530

    Rotate Array

--- Array/rotateArray.py
+++ Array/rotateArray.py
@@ -1,10 +1,10 @@
 class Solution:
-    def rev(self, nums):
-        l, r = 0, len(nums)-1
-
+    def rev(self, nums, l, r):
         while l<r:
             nums[l], nums[r] = nums[r], nums[l]
             l, r = l+1, r-1
 
     def rotatearray(self, nums, k):
-        pass
+        self.rev(nums, 0, len(nums)-k)
+        self.rev(nums, len(nums)-k, len(nums)-1)
+        self.rev(nums, 0, len(nums)-1)
