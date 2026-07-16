e70f1cd6b57d7aaaaac07b3f8085b31e5b45ce40
ParthKhare21 <khare.parth2112@gmail.com>
2026-07-16 23:28:06 +0530

    Learning DSA in JAVA

--- Basic Maths/Power.java
+++ Basic Maths/Power.java
@@ -1,4 +1,8 @@
 public class Power {
+    public static int powerIterative(int x, int n){
+        if (n==0) return 1;
+        if (n==1) return x;
+    }
     public static void main(String args[]){
         
     }
