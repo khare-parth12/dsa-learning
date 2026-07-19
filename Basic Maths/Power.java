7e2a72aafce466acb13e403660cc684922d5aa60
ParthKhare21 <khare.parth2112@gmail.com>
2026-07-19 23:08:40 +0530

    DSA in JAVA - Power problem using Iterative approach

--- Basic Maths/Power.java
+++ Basic Maths/Power.java
@@ -1,9 +1,23 @@
 public class Power {
-    public static int powerIterative(int x, int n){
-        if (n==0) return 1;
-        if (n==1) return x;
+    public static double powerIterative(double x, int n) {
+        long N = n;
+        if (N < 0){
+            x = 1.0/x;
+            N = -N;
+        }
+
+        double result = 1;
+        while (N > 0) {
+            if (N%2==1) result = result * x;
+            x *= x;
+            N /= 2;
+        }
+
+        return result;
     }
+
     public static void main(String args[]){
-        
+        System.out.println(powerIterative(5, 4));
+        System.out.println(powerIterative(5, -4));
     }
 }
