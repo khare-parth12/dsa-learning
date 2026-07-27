94fa8d39177129d849077a08d2d2bf339ca7163d
ParthKhare21 <khare.parth2112@gmail.com>
2026-07-27 23:12:57 +0530

    Strings in JAVA

---
+++ Strings/ReverseWord.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class ReverseWord {
+    public static String reverseWord(String s){
+        return "a";
+    }
+    public static void main(String[] args) {
+        
+    }
+    
+}
