4f93822d1abe2db090720dd65650df70a6927758
ParthKhare21 <khare.parth2112@gmail.com>
2026-08-01 23:39:46 +0530

    DSA in JAVA - Reverse String

--- Strings/ReverseWord.java
+++ Strings/ReverseWord.java
@@ -2,20 +2,20 @@ package Strings;
 
 public class ReverseWord {
     public static String reverseWord(String s){
-        if (s == null) return "";
-        if (s.length() == 0 || s.length() == 1) return s;
-        int left = 0;
-        int right = s.length() - 1;
-
-        while (left < right) {
-            
-            left++;
-            right--;
+        if (s == null || s.isEmpty()) return s;
+        if (s.length() == 1) return s;
+                
+        int j = 0;
+        char[] str1 = new char[s.length()];
+        for (int i=s.length()-1; i>=0; i--){
+            str1[j] = s.charAt(i);
+            j++;
         }
-        return true;
+        return new String(str1);
     }
+
     public static void main(String[] args) {
-        
+        System.out.println(reverseWord("Parth"));
     }
     
 }
